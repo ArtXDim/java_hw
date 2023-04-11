d75e86ec1085f1f800fc7060c07b3d4eba20bf59
Artemiev Dima <adbart1985@gmail.com>
2023-04-11 08:07:34 +0300

    solved HW4

--- task4_3.java
+++ task4_3.java
@@ -1,9 +1,7 @@
-// Task4_3
+/*
+ * Task_5
 import java.util.Deque;
-import java.util.*;
-
-public class task4_3 {
-
+public class Task_5 {
 //Дана строка содержащая только символы '(', ')', '{', '}', '[' и ']', определите,
 // является ли входная строка логически правильной.
 // Входная строка логически правильная, если:
@@ -15,14 +13,47 @@ public class task4_3 {
 // {[()]} = true
 // ()() = true
 // )()( = false
-
 public static void main(String[] args) {
-
 }
-
 public boolean validate(Deque<Integer> deque){
-
     return false;
 }
 }
+ */
+import java.util.Scanner;
+import java.util.Stack;
 
+ public class task4_3 {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Введите строку, содержащую только символы '(', ')', '{', '}', '[' и ']': ");
+        String inputString = scanner.nextLine();
+
+        Stack<Character> stack = new Stack<>();
+
+        for (int i = 0; i < inputString.length(); i++) {
+            char c = inputString.charAt(i);
+
+            if (c == '(' || c == '[' || c == '{') {
+                stack.push(c);
+            } else if (c == ')' && !stack.isEmpty() && stack.peek() == '(') {
+                stack.pop();
+            } else if (c == ']' && !stack.isEmpty() && stack.peek() == '[') {
+                stack.pop();
+            } else if (c == '}' && !stack.isEmpty() && stack.peek() == '{') {
+                stack.pop();
+            } else {
+                System.out.println("Введенная строка логически неправильная");
+                return;
+            }
+        }
+
+        if (stack.isEmpty()) {
+            System.out.println("Введенная строка логически правильная");
+        } else {
+            System.out.println("Введенная строка логически неправильная");
+        }
+    }
+}
